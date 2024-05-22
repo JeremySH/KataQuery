effe9c92b700159ced06a3e72a27d120bd58d339
JeremySH <JeremySH@users.noreply.github.com>
2024-05-22 18:44:13 -0500

    add localized_policy example script
    localized_policy.py shows how one can massage policy values when "Localize" is enabled, in order to see familiar numbers from position to position

---
+++ script_examples/analysis/localized_policy.py
@@ -0,0 +1,43 @@
+# Localized Policy
+# get similar "policy" values
+# when localize is used
+
+# here, localized policies are rescaled to their 
+# own domain instead of the entire board, 
+# which gives a more consistent impression
+# of value
+
+# use the checkbox and Localize... menu to observe effect
+
+LOCALIZE_POLICY = check1("localize pol", default_value=True)
+MOVES_PER_STONE = slider1("moves per stone", min_value=1, max_value=10, default_value=3, value_type="int")
+
+clearLog()
+log(f"Moves Per Stone: {MOVES_PER_STONE}")
+
+def normalizePolicy(ans):
+	"renormalize policy to allowed moves"
+	sumPol = sum(p.policy for p in ans.allowed_moves)
+	for m in ans.moves_by_policy:
+		m.policy = m.policy/sumPol
+
+if LOCALIZE_POLICY:
+	normalizePolicy(k)
+	
+clearAll()
+
+ourMoves = sorted(k.allowed_moves, key=lambda m: -m.policy)
+
+limit = max(len(k.stones)*MOVES_PER_STONE, MOVES_PER_STONE)
+
+for m in ourMoves[:limit]:
+	p = round(len(k.legal_moves) * m.policy)
+	mark(m, p)
+
+status(f"{k.toPlay} to play")
+
+# note that we multiply by len(k.legal_moves) instead
+# of len(k.allowed_moves) for the simple reason
+# that the number remains similar whether you have
+# localize turned on or off
+
