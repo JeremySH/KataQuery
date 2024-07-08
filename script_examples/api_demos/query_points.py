4028a3dc4f43352ff3f6964de27b0b6d398dbef7
JeremySH <JeremySH@users.noreply.github.com>
2024-07-07 21:04:03 -0500

    simplify query_points.py example
    just show a minimal example, as this is more instructive

--- script_examples/api_demos/query_points.py
+++ script_examples/api_demos/query_points.py
@@ -6,43 +6,18 @@
 # This is useful for triggering deeper analysis
 # or for creating games/trainers
 
-# Points can be specified via Query Mode
-# or a simple middle-click of the mouse.
-		
-def localFight(point):
-	"show a possible local fight near point"
-	g = k.goban.copy()
-	ans = k
-	plays = []
-	thePlay = point
-	
-	# show 9 local moves
-	for x in range(1,10):
-		# play current move
-		g.play(g.toPlay, thePlay)
-		ghost(thePlay, g.toPlay)
-		mark(thePlay, x)
-		plays.append(thePlay)
-		
-		# analyze for next move
-		allowed = plays
-		for _ in range(3):
-			allowed = g.nearby_these(allowed)
-		
-		g.toPlay = opponent(g.toPlay)
-		ans = analyze(g, allowedMoves=allowed)
-		thePlay = ans.bestMove
-	
+# Points can be specified using Query Mode
+# or with amiddle-click of the mouse.
+
+# In Query mode:
+# Click to create one query_point
+# Shift-click to add query_points
+# Right-click to delete a query_point
+
 clearAll()
-if k.depth == "full":
-	# use the first legal query point
-	# to generate a local fight
-	if len(k.query_points):
-		play = None
-		for p in k.query_points:
-			if p.legal:
-				play = p
-				break
-		
-		if play:
-			localFight(k.query_points[0])
+
+for q in k.query_points:
+	# do amazing calculations here
+	hover(q, f"ownership: {q.ownership}")
+	mark(q, "square")
+
