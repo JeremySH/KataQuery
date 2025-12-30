6e3f22d0b8b91904b47eb6a8186b27db9f790dfe
JeremySH <JeremySH@users.noreply.github.com>
2025-12-29 23:07:50 -0600

    Allow human to play against certainty_bot.py

--- script_examples/miscellaneous/certainty_bot.py
+++ script_examples/miscellaneous/certainty_bot.py
@@ -12,16 +12,21 @@
 # uncertainty  = k.bestMove.utility- k.bestMove.utilityLcb
 # certainty    = 1.0 - uncertainty
 
+persist("HUMAN", "black")
 VISIT_LIMIT = 3000
-CERTAINTY = slider1("certainty", default_value=0.75)
 
-log("Certainty: {CERTAINTY*100:0.1}")
+CERTAINTY = slider1("certainty", default_value=0.75)
+CHOOSE_HUMAN = button1("Player...")
+
+if CHOOSE_HUMAN:
+	b = msgBox("Who do you play?", buttons=["black", "white", "neither"])
+	HUMAN = b
 
 def analyze_til_error(goban, error, maxVisits, allowedMoves=None, ghostUpdates=True):
 	"""
 	analyze `goban` until bestMove.utility - bestMove.utilityLcb < error, 
 	giving up after `maxVisits`.
-	return (kataanswer, error_factor) with this analysis
+	return (kataanswer, error_utility, error_winrate) with this analysis
 	"""
 	# visits are added in chunks of 50
 	# so that the final error value "kinda" hits the same spot
@@ -59,6 +64,11 @@ def analyze_til_error(goban, error, maxVisits, allowedMoves=None, ghostUpdates=T
 
 clearLog()
 log(f"Minimum Certainty: {CERTAINTY*100:0.1f}")
+
+if k.player == HUMAN:
+	clearAll()
+	status(f"{HUMAN} to play.")
+	bail()
 
 if k.depth == "full":
 	allowed = [m.pos for m in k.allowed_moves]
