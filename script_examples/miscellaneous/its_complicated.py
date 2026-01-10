aea202788abfae25f1319c8e5c16df2436982460
JeremySH <JeremySH@users.noreply.github.com>
2026-01-09 19:35:02 -0600

    Make its_complicated.py quicker
    Just use move.Stdev in the provided move selection instead of deep-analyzing each available move. It's quicker and the style is similar. Also, increase visits during GEN_GAME to get a larger move selection.

--- script_examples/miscellaneous/its_complicated.py
+++ script_examples/miscellaneous/its_complicated.py
@@ -1,49 +1,53 @@
 # "It's Complicated" Bot
+# recommended:
+# Network: b15
+# Pure:    OFF
 
 # This bot chooses the move that produces the most complexity,
-# as implied by k.scoreStdDev
+# as implied by move.scoreStdDev
 # (see complex_move())
 
 # when the evaluation is inverted,
 # bot tends to simplify the board
 
-GEN_GAME = button1("Gen Game")
+import random
+
 persist("HUMAN", "neither")
+
+GEN_GAME =       button1("Gen Game")
 HUMAN_SELECTED = button2("Player...")
-MARK_BEST = check1("Mark Best Move")
-INVERT = check2("Simplify instead")
+MARK_BEST =      check1("Mark Best Move")
+INVERT =         check2("Simplify instead")
 
-SELECT_PROB = 1.0  # ajdust lower to get different games
-
+SELECT_PROB = 0.75  # how likely to go complicated
+	
 def complex_move(ans, invert=False):
 	"""
 	return a moveinfo that represents
-	the move that creates the most complexity.
+	the move that creates the most complexity
+	(within reason).
 	if `invert`, return a move with least complex result.
 	"""
+	import random
+	SLACK = 0.1 # for a bit of randomness
+	
 	# don't go too crazy
 	if ans.bestMove.winrate < .45:
 		return ans.bestMove
-	
-	#moves = ans.filter(f"{ans.bestMove.scoreLead} - m.scoreLead < 0.5", ans.moves)
-	moves = ans.filter(f"{ans.bestMove.winrate} - m.winrate < 0.03", ans.moves)
-	
-	for m in moves:
-		# try the move
-		g = ans.goban.copy()
-		g.play(ans.player, m)
-		g.player = opponent(g.player)
-		
-		# quickly calculate complexity (scoreStdev)
-		a = analyze(g, visits = 2)
-		m['complexity'] = a.scoreStdev
-	
-	if invert:
-		return ans.min("m.complexity", moves)
-		
-	return ans.max("m.complexity", moves)
 
-import random
+	moves = ans.filter(f"{ans.bestMove.winrate} - m.winrate < 0.02", ans.moves)
+		
+	if invert:
+		moves = ans.sorted("m.scoreStdev", moves)
+	else:
+		moves = ans.sorted("-m.scoreStdev", moves)
+	
+	m = moves.pop(0)
+	
+	while random.random() < SLACK and len(moves):
+		m = moves.pop(0)
+
+	return m
 
 if HUMAN_SELECTED:
 	HUMAN = msgBox("Who do you play?", buttons=["black", "white", "neither"])
@@ -60,7 +64,7 @@ if GEN_GAME:
 	g.player = "black"
 	
 	for p in range(moveCount):
-		ans = analyze(g, visits=10)
+		ans = analyze(g, visits=30)
 		move = ans.bestMove
 		
 		if random.random() <= SELECT_PROB:
@@ -74,6 +78,7 @@ if GEN_GAME:
 	bookmark(g, location="end")
 	msgBox("New Game bookmarked!")
 
+# generate a bot move if appropriate
 if k.player != HUMAN:	
 	if k.depth == "full":
 		complicated = k.bestMove
